aeb4a7cf720b4fcf9c3f1395b74a1353d39e5557
FERAS ABDALRAHMAN <doonfrs@gmail.com>
2023-05-17 22:49:06 +0300

    initial commit

---
+++ lib/flutter_quill2.dart
@@ -0,0 +1,276 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:filesystem_picker/filesystem_picker.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_quill/extensions.dart';
+import 'package:flutter_quill/flutter_quill.dart' hide Text;
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'flutter_quill_extensions/flutter_quill_extensions.dart';
+
+enum _SelectionType {
+  none,
+  word,
+  // line,
+}
+
+class FlutterQuill2 extends StatefulWidget {
+  @override
+  _FlutterQuill2State createState() => _FlutterQuill2State();
+}
+
+class _FlutterQuill2State extends State<FlutterQuill2> {
+  final QuillController _controller = QuillController.basic();
+  final FocusNode _focusNode = FocusNode();
+  Timer? _selectAllTimer;
+  _SelectionType _selectionType = _SelectionType.none;
+
+  @override
+  void dispose() {
+    _selectAllTimer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildEditor(context);
+  }
+
+  bool _onTripleClickSelection() {
+    _selectAllTimer?.cancel();
+    _selectAllTimer = null;
+
+    if (_controller.selection.isCollapsed) {
+      _selectionType = _SelectionType.none;
+    }
+
+    if (_selectionType == _SelectionType.none) {
+      _selectionType = _SelectionType.word;
+      _startTripleClickTimer();
+      return false;
+    }
+
+    if (_selectionType == _SelectionType.word) {
+      final child = _controller.document.queryChild(
+        _controller.selection.baseOffset,
+      );
+      final offset = child.node?.documentOffset ?? 0;
+      final length = child.node?.length ?? 0;
+
+      final selection = TextSelection(
+        baseOffset: offset,
+        extentOffset: offset + length,
+      );
+
+      _controller.updateSelection(selection, ChangeSource.REMOTE);
+
+      _selectionType = _SelectionType.none;
+
+      _startTripleClickTimer();
+
+      return true;
+    }
+
+    return false;
+  }
+
+  void _startTripleClickTimer() {
+    _selectAllTimer = Timer(const Duration(milliseconds: 900), () {
+      _selectionType = _SelectionType.none;
+    });
+  }
+
+  Widget _buildEditor(BuildContext context) {
+    final quillEditor = MouseRegion(
+      cursor: SystemMouseCursors.text,
+      child: QuillEditor(
+        controller: _controller,
+        scrollController: ScrollController(),
+        scrollable: true,
+        focusNode: _focusNode,
+        autoFocus: false,
+        readOnly: false,
+        placeholder: 'Add content',
+        enableSelectionToolbar: isMobile(),
+        expands: false,
+        padding: EdgeInsets.zero,
+        onImagePaste: _onImagePaste,
+        onTapUp: (details, p1) {
+          return _onTripleClickSelection();
+        },
+        customStyles: DefaultStyles(
+          h1: DefaultTextBlockStyle(
+              const TextStyle(
+                fontSize: 32,
+                color: Colors.black,
+                height: 1.15,
+                fontWeight: FontWeight.w300,
+              ),
+              const VerticalSpacing(16, 0),
+              const VerticalSpacing(0, 0),
+              null),
+          sizeSmall: const TextStyle(fontSize: 9),
+        ),
+        embedBuilders: [
+          ...FlutterQuillEmbeds.builders(),
+        ],
+      ),
+    );
+
+    final toolbar = QuillToolbar.basic(
+      showDirection: true,
+      controller: _controller,
+      embedButtons: FlutterQuillEmbeds.buttons(
+        // provide a callback to enable picking images from device.
+        // if omit, "image" button only allows adding images from url.
+        // same goes for videos.
+        onImagePickCallback: _onImagePickCallback,
+        onVideoPickCallback: _onVideoPickCallback,
+        webImagePickImpl: kIsWeb ? _webImagePickImpl : null,
+        filePickImpl: isDesktop() ? openFileSystemPickerForDesktop : null,
+        // uncomment to provide a custom "pick from" dialog.
+        // mediaPickSettingSelector: _selectMediaPickSetting,
+        // uncomment to provide a custom "pick from" dialog.
+        // cameraPickSettingSelector: _selectCameraPickSetting,
+      ),
+      showAlignmentButtons: true,
+      afterButtonPressed: _focusNode.requestFocus,
+    );
+
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            flex: 15,
+            child: Container(
+              color: Colors.white,
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: quillEditor,
+            ),
+          ),
+          kIsWeb
+              ? Expanded(
+                  child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                  child: toolbar,
+                ))
+              : Container(child: toolbar)
+        ],
+      ),
+    );
+  }
+
+  Future<String?> openFileSystemPickerForDesktop(BuildContext context) async {
+    return await FilesystemPicker.open(
+      context: context,
+      rootDirectory: await getApplicationDocumentsDirectory(),
+      fsType: FilesystemType.file,
+      fileTileSelectMode: FileTileSelectMode.wholeTile,
+    );
+  }
+
+  // Renders the image picked by imagePicker from local file storage
+  // You can also upload the picked image to any server (eg : AWS s3
+  // or Firebase) and then return the uploaded image URL.
+  Future<String> _onImagePickCallback(File file) async {
+    // Copies the picked file from temporary cache to applications directory
+    final appDocDir = await getApplicationDocumentsDirectory();
+    final copiedFile =
+        await file.copy('${appDocDir.path}/${basename(file.path)}');
+    return copiedFile.path.toString();
+  }
+
+  Future<String?> _webImagePickImpl(
+      OnImagePickCallback onImagePickCallback) async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) {
+      return null;
+    }
+
+    // Take first, because we don't allow picking multiple files.
+    final fileName = result.files.first.name;
+    final file = File(fileName);
+
+    return onImagePickCallback(file);
+  }
+
+  // Renders the video picked by imagePicker from local file storage
+  // You can also upload the picked video to any server (eg : AWS s3
+  // or Firebase) and then return the uploaded video URL.
+  Future<String> _onVideoPickCallback(File file) async {
+    // Copies the picked file from temporary cache to applications directory
+    final appDocDir = await getApplicationDocumentsDirectory();
+    final copiedFile =
+        await file.copy('${appDocDir.path}/${basename(file.path)}');
+    return copiedFile.path.toString();
+  }
+
+  // ignore: unused_element
+  Future<MediaPickSetting?> _selectMediaPickSetting(BuildContext context) =>
+      showDialog<MediaPickSetting>(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          contentPadding: EdgeInsets.zero,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextButton.icon(
+                icon: const Icon(Icons.collections),
+                label: const Text('Gallery'),
+                onPressed: () => Navigator.pop(ctx, MediaPickSetting.Gallery),
+              ),
+              TextButton.icon(
+                icon: const Icon(Icons.link),
+                label: const Text('Link'),
+                onPressed: () => Navigator.pop(ctx, MediaPickSetting.Link),
+              )
+            ],
+          ),
+        ),
+      );
+
+  // ignore: unused_element
+  Future<MediaPickSetting?> _selectCameraPickSetting(BuildContext context) =>
+      showDialog<MediaPickSetting>(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          contentPadding: EdgeInsets.zero,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextButton.icon(
+                icon: const Icon(Icons.camera),
+                label: const Text('Capture a photo'),
+                onPressed: () => Navigator.pop(ctx, MediaPickSetting.Camera),
+              ),
+              TextButton.icon(
+                icon: const Icon(Icons.video_call),
+                label: const Text('Capture a video'),
+                onPressed: () => Navigator.pop(ctx, MediaPickSetting.Video),
+              )
+            ],
+          ),
+        ),
+      );
+
+  Future<String> _onImagePaste(Uint8List imageBytes) async {
+    // Saves the image to applications directory
+    final appDocDir = await getApplicationDocumentsDirectory();
+    final file = await File(
+            '${appDocDir.path}/${basename('${DateTime.now().millisecondsSinceEpoch}.png')}')
+        .writeAsBytes(imageBytes, flush: true);
+    return file.path.toString();
+  }
+}
